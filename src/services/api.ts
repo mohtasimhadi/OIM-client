231ffbe2c78c5d03aa510779539bea25eda4f8ea
Mohtasim Hadi Rafi <mzr0167@auburn.edu>
2024-10-07 12:21:25 -0500

    port changed for api

--- src/services/api.ts
+++ src/services/api.ts
@@ -16,7 +16,7 @@ export const uploadVideos = async (videoInfo: {
     });
   
     try {
-      const response = await fetch('http://localhost:8000/upload/', {
+      const response = await fetch('http://localhost:8080/upload/', {
         method: 'POST',
         body: formData,
       });
