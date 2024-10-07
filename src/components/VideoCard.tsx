8f9f26595eed2bc2a30cfa31780788324bfd19b3
Mohtasim Hadi Rafi <mzr0167@auburn.edu>
2024-10-07 14:27:29 -0500

    video card size fixed

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -8,7 +8,7 @@ interface VideoCardProps {
 const VideoCard: React.FC<VideoCardProps> = ({ title, videoSrc }) => (
   <div>
     <p className="mb-2 text-center font-semibold">{title}</p>
-    <video className="w-full h-48 rounded-md border-2 border-gray-200" controls>
+    <video className="w-full aspect-[3/1.7] rounded-md border-2 border-gray-200" controls>
       <source src={videoSrc} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
