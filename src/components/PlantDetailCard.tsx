95fe8811eab73bbab77206f4c887207e9a514b7a
Mohtasim Hadi Rafi <mzr0167@auburn.edu>
2024-10-08 13:35:33 -0500

    api address changes

--- src/components/PlantDetailCard.tsx
+++ src/components/PlantDetailCard.tsx
@@ -71,7 +71,7 @@ const PlantDetailCard: React.FC<PlantDetailCardProps> = ({
 
     {/* Left Column - Plant Image */}
     <div className="w-1/3 h-35">
-      <img src={`http://10.33.9.30:8000/image/view/${image}`} alt="Plant" className="w-full aspect-square rounded-md shadow-sm" />
+      <img src={`http://172.19.214.198:8000/image/view/${image}`} alt="Plant" className="w-full aspect-square rounded-md shadow-sm" />
     </div>
 
     {/* Right Column - Details */}
