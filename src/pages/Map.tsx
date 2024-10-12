4e155db690c132a95758632dcd3ff945f2852e69
Mohtasim Hadi Rafi <mzr0167@auburn.edu>
2024-10-12 17:35:28 -0500

    initial map

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import MapView from '@arcgis/core/views/MapView';
+import WebMap from '@arcgis/core/WebMap';
 
-const Map: React.FC = () => {
+const ArcGISMap = () => {
+  const mapDiv = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const webMap = new WebMap({
+      portalItem: {
+        id: 'd7e999d5193942759db61dd40644a76a', // WebMap ID
+      },
+    });
+
+    const view = new MapView({
+      container: mapDiv.current as HTMLDivElement,
+      map: webMap,
+      rotation: 90, // Set the maximum zoom-out level (larger scale number)
+    });
+
+    return () => {
+      if (view) {
+        view.destroy(); // Clean up the MapView when the component is unmounted
+      }
+    };
+  }, []);
+
   return (
-    <div className={`p-4 min-h-full`}>
-      <div className="container mx-auto space-y-6">
-
-      </div>
+    <div className="flex items-center justify-center w-full h-full">
+      <div ref={mapDiv} className="w-full h-[600px] m-4"></div>
     </div>
   );
 };
 
-export default Map;
+export default ArcGISMap;
--- src/pages/RoverDash.tsx
+++ src/pages/RoverDash.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const Map: React.FC = () => {
+const PolygonDiv = () => {
   return (
-    <div className={`p-4 min-h-full`}>
-      <div className="container mx-auto space-y-6">
-
-      </div>
+    <div className="flex items-center justify-center w-full h-full">
+      <div
+        className="w-[300px] h-[300px] bg-blue-500"
+        style={{
+          clipPath: 'polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)',
+        }}
+      ></div>
     </div>
   );
 };
 
-export default Map;
+export default PolygonDiv;
